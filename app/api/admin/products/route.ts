90496b4df0262716143b2d94736918248dea17a7
Lebahati <lebahatijohn@.com>
2026-04-12 11:55:50 +0300

    fix params

--- app/api/admin/products/route.ts
+++ app/api/admin/products/route.ts
@@ -33,3 +33,24 @@ export async function POST(req: NextRequest) {
     );
   }
 }
+
+export async function DELETE(
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> }
+) {
+  try {
+    const { id } = await context.params;
+
+    await prisma.product.delete({
+      where: { id },
+    });
+
+    return NextResponse.json({ message: "Deleted" });
+  } catch (error) {
+    console.error("DELETION ERROR:", error);
+    return NextResponse.json(
+      { error: "Delete failed" },
+      { status: 500 }
+    );
+  }
+}
