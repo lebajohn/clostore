87ce07c25fbe1a9453db9785f62bf49f871b8c2b
Lebahati <lebahatijohn@.com>
2026-04-12 12:01:35 +0300

    fit delete place

--- app/api/admin/products/[id]/route.ts
+++ app/api/admin/products/[id]/route.ts
@@ -1,24 +1,24 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { prisma } from "@/db/prisma";
 
 
 export async function DELETE(
-    req: Request,
-    { params }: { params: { id: string } }
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> }
 ) {
-    try {
-        await prisma.product.delete({
-            where: {
-                id: params.id,
-            },
-        });
+  try {
+    const { id } = await context.params;
 
-        return NextResponse.json({ message: "Deleted" });
-    } catch (error) {
-        console.error("PRODUCT FETCH ERROR:", error);
-        return NextResponse.json(
-            { error: "Delete Failed" },
-            { status: 500}
-        );
-    }
+    await prisma.product.delete({
+      where: { id },
+    });
+
+    return NextResponse.json({ message: "Deleted" });
+  } catch (error) {
+    console.error("DELETION ERROR:", error);
+    return NextResponse.json(
+      { error: "Delete failed" },
+      { status: 500 }
+    );
+  }
 }
